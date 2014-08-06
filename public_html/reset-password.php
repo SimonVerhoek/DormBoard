e9332925d9d93b3ea7b49d9bba83572529b1b584
Simon Verhoek <saverhoek@gmail.com>
2014-08-06 14:43:08 +0200

    reset-password backend

--- public_html/reset-password.php
+++ public_html/reset-password.php
@@ -25,10 +25,36 @@
             errorMsg('Make sure that what you fill in at "new password" and "confirm new password" are equal to each other!');
         }
 
-    	// if old password is correct
-    		// hash new password
-        	// replace old hash with new hash
+        $users = query("SELECT 	hash
+        				FROM 	users
+    					WHERE 	user_id = ?",
+    							$_SESSION["user_id"]);
 
+        if (count($users) == 1)
+        {
+        	$user = $users[0];
+
+        	// compare hashes
+        	if (crypt($_POST["password-old"], $user["hash"]) == $user["hash"])
+        	{
+        		// hash new password
+        		$newHash = crypt($_POST["password-new"]);
+
+        		// replace old hash with new hash
+        		$updatePassword = query("	UPDATE 	users
+        									SET 	hash = ?",
+        									$newHash);
+
+        		if ($updatePassword === false)
+        		{
+        			errorMsg("Failed to set new password. Please try again.");
+        		}
+        		else
+        		{
+        			redirect("profile.php");
+        		}
+        	}
+        }
 
     }
 ***REMOVED***
