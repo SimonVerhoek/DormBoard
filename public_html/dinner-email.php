9c4e28943a4d4fe12ed78b5e3e15c4e85d90638c
Simon Verhoek <saverhoek@gmail.com>
2014-08-08 16:35:03 +0200

    created first draft of dinner-email.php

---
+++ public_html/dinner-email.php
@@ -0,0 +1,61 @@
+***REMOVED***
+    // configuration
+    require("../includes/config.php"); 
+    require_once("PHPMailer/class.phpmailer.php");
+    
+    $user = query(" SELECT  first_name,
+                            dorm_id
+                    FROM    users  
+                    WHERE   user_id = ?",
+                            $_SESSION["user_id"]);
+
+    $roommates = query("SELECT  user_id,
+                                first_name,
+                                email
+                        FROM    users
+                        WHERE   dorm_id = ?",
+                                $user[0]["dorm_id"]);
+
+    // make array of roommates' email addresses
+    $emailAddresses = [];
+
+    foreach ($roommates as $roommate) 
+    {
+        if ($roommate["user_id"] != $_SESSION["user_id"]) 
+        {
+            array_push($emailAddresses, $roommate["email"]);
+        }
+    }
+    
+    // instantiate mailer
+    $mail = new PHPMailer();
+
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465;
+    $mail->SMTPSecure = 'ssl';
+    $mail->Username = "dormboardmail@gmail.com";  
+    $mail->Password = "ywK-G9s-H5X-hfS";          
+
+    // set From:
+    $mail->SetFrom("dormboardmail@gmail.com");
+
+    // set To:
+    $mail->AddAddress("saverhoek@gmail.com");
+
+    // set Subject:
+    $mail->Subject = "Dormboard: Somebody's cooking tonight!";
+
+    // set body
+    $mail->Body = 
+        "
+        TEST
+";
+
+    // send mail
+    if ($mail->Send() === false)
+        die($mail->ErrorInfo . "\n");
+    
+    redirect("dinner.php");
+***REMOVED***
