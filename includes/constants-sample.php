10bd4654eafb0861c1db29f0fef8fac0a1030a31
Simon Verhoek <saverhoek@gmail.com>
2014-08-26 20:36:16 +0200

    added constants-sample.php

---
+++ includes/constants-sample.php
@@ -0,0 +1,33 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     * Global constants.
+     *
+     * To make the application work:
+     * - change the name of this file into constants.php
+     * - fill in your own credentials at every ***_***
+     */ 
+
+    define("WEBSITEROOT", "*** root of your website ***");
+
+    // mySQL database name
+    define("DATABASE", "*** name of your mySQL database ***");
+
+    // mySQL database password
+    define("PASSWORD", "*** mySQL database password ***"); // remote
+
+    // mySQL database server
+    define("SERVER", "*** mySQL database host address ***"); // remote
+
+    // mySQL database username
+    define("USERNAME", "*** mySQL database username ***"); // remote
+
+    // PHPMailer 
+    // *** email service used *** - username
+    define("DINNER_EMAIL_ADDRESS", "*** email address ***");
+
+    // *** email service used *** - password
+    define("DINNER_EMAIL_PASSWORD", "*** email password ***")
+?>
