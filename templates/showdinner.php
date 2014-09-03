8946d7bde859230b1c434b1baf011f64472d3e9d
Simon Verhoek <saverhoek@gmail.com>
2014-09-03 23:48:02 +0200

    removed panel from showdinner

--- templates/showdinner.php
+++ templates/showdinner.php
@@ -1,10 +1,3 @@
-<!--
- |
- |  Shows a weekly schedule of who's cooking and who's
- |	joining dinner.
- |
- -->
-
 <script type="text/javascript">
 
 	// make Dinner tab active
@@ -14,114 +7,109 @@
 	});
 </script>
 
-<!-- show schedule -->
+<!-- Dinner schedule -->
 <div class="col-xs-10 column" id="dinner-column">
 
-	<div class="panel">
+	<div class="tab-header">
 
-		<div class="panel-heading">
-			<div class="panel-header-title">
-				<p class="lead">Dinner Schedule</p>
-			</div>
-		
+		<h1>Dinner Schedule</h1>
 
-			<div class="panel-header-rest">
-				<?php 
-					build("dinner_form.php", [
-						"days" => $days
-						]) ;
-				?>
-			</div>
+		<div>
+			<?php 
+				build("dinner_form.php", [
+					"days" => $days
+					]) ;
+			?>
 		</div>
+	</div>
 
-		<div class="panel-body">
+	<div class="tab-content">
 
-			<table class="table table-condensed" id="dinner-table">
-				<thead id="dinner-table-header">
-					<tr>
-						<td></td>
-						<?php 
-							foreach ($days as $i => $day) 
-							{
-								echo "<th>";
-								switch ($i) 
-						    	{
-						    		case 0:
-						    			echo ("Today");
-						    			break;
-						    		case 1:
-						    			echo ("Tomorrow");
-						    			break;
-						    		default:
-						    			echo ($day->format('l'));
-						    			break;
-						    	}
-						    	echo "<br>";
-
-						    	echo $day->format("F jS");
-
-						    	echo "</th>";
-							}
-						?>
-					</tr>
-				</thead>
-				<tbody id="dinner-table-body">
-					<?php
-						// for every roommate
-						foreach ($roommates as $roommate)
+		<table class="table table-condensed" id="dinner-table">
+			<thead id="dinner-table-header">
+				<tr>
+					<td></td>
+					<?php 
+						foreach ($days as $i => $day) 
 						{
-							// print roommate's name
-							echo "<tr id='dinner-row'>";
-					    	echo(	"<td id='rm-names'>" . 
-					    			$roommate["first_name"] .  
-					    			"</td>"
-				    			);
+							echo "<th>";
+							switch ($i) 
+					    	{
+					    		case 0:
+					    			echo ("Today");
+					    			break;
+					    		case 1:
+					    			echo ("Tomorrow");
+					    			break;
+					    		default:
+					    			echo ($day->format('l'));
+					    			break;
+					    	}
+					    	echo "<br>";
 
-					    	// for every upcoming day
-					    	foreach ($days as $day) 
-							{ 
-						    	echo "<td>";
+					    	echo $day->format("F jS");
 
-						    	$today = $day->format('Y-m-d');
-
-						    	// for every roommate's status
-						    	foreach ($statuses as $status)
-						    	{
-						    		// if status is from this user &
-						    		// status is from this day
-							    	if (($status["user_id"] === $roommate["user_id"]) && 
-							    		($status["action_date"] === $today))
-							    	{
-						    			// show appropriate text
-						    			switch ($status["status"])
-						    			{
-						    				case '1':
-						    					echo "<img class='icon' src=" . WEBSITEROOT . "/img/cook3.png>";
-						    					break;
-					    					case '2':
-					    						echo "<img class='icon' src=" . WEBSITEROOT . "/img/join3.png>";
-					    						break;
-				    						case '3':
-				    							echo "<img class='icon' src=" . WEBSITEROOT . "/img/notjoin.png>";
-						    				default:
-						    					// print nothing
-						    					break;
-						    			}
-							    	}
-						    	}
-
-								echo "</td>";	
-							} 
-
-							echo "</tr>";
-						}	
+					    	echo "</th>";
+						}
 					?>
-				</tbody>
-			</table>
+				</tr>
+			</thead>
+			<tbody id="dinner-table-body">
+				<?php
+					// for every roommate
+					foreach ($roommates as $roommate)
+					{
+						// print roommate's name
+						echo "<tr id='dinner-row'>";
+				    	echo(	"<td id='rm-names'>" . 
+				    			$roommate["first_name"] .  
+				    			"</td>"
+			    			);
 
-		</div> <!-- closes panel-body -->
-	</div> <!-- closes panel -->
+				    	// for every upcoming day
+				    	foreach ($days as $day) 
+						{ 
+					    	echo "<td>";
 
-</div> <!-- closes column -->
+					    	$today = $day->format('Y-m-d');
+
+					    	// for every roommate's status
+					    	foreach ($statuses as $status)
+					    	{
+					    		// if status is from this user &
+					    		// status is from this day
+						    	if (($status["user_id"] === $roommate["user_id"]) && 
+						    		($status["action_date"] === $today))
+						    	{
+					    			// show appropriate text
+					    			switch ($status["status"])
+					    			{
+					    				case '1':
+					    					echo "<img class='icon' src=" . WEBSITEROOT . "/img/cook3.png>";
+					    					break;
+				    					case '2':
+				    						echo "<img class='icon' src=" . WEBSITEROOT . "/img/join3.png>";
+				    						break;
+			    						case '3':
+			    							echo "<img class='icon' src=" . WEBSITEROOT . "/img/notjoin.png>";
+					    				default:
+					    					// print nothing
+					    					break;
+					    			}
+						    	}
+					    	}
+
+							echo "</td>";	
+						} 
+
+						echo "</tr>";
+					}	
+				?>
+			</tbody>
+		</table>
+
+	</div> <!-- ./ .tab-content -->
+
+</div> <!-- ./ dinner-column -->
 
 
