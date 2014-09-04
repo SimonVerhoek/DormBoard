48716cf5ea6885cd6b0a7b1ff95a9ef4c098605e
Simon Verhoek <saverhoek@gmail.com>
2014-09-04 17:49:25 +0200

    changed css roommates list & dorm name

--- templates/shownavbar.php
+++ templates/shownavbar.php
@@ -6,32 +6,35 @@
 
 <div class="col-xs-2 column" id="nav-column">
 
-	<!-- navigation -->
-	<div class="nav nav-pills nav-stacked" id="nav-tab-buttons" role="navigation">
-	    <li id="navdinner">   <a href="dinner.php">	  dinner		</a></li>
-	    <li id="navshoplist"> <a href="shoplist.php"> Shopping List </a></li>
-	    <li id="navfinances"> <a href="finances.php"> finances		</a></li>
+	<div id="nav-and-dorm">
+
+		<h2 id="dorm-name" align="center"><?= $dorm[0]["dorm_name"] ?></h2>
+
+		<!-- navigation -->
+		<div class="nav nav-pills nav-stacked" id="nav-tab-buttons" role="navigation">
+		    <li id="navdinner">   <a href="dinner.php">	  dinner		</a></li>
+		    <li id="navshoplist"> <a href="shoplist.php"> Shopping List </a></li>
+		    <li id="navfinances"> <a href="finances.php"> finances		</a></li>
+		</div>
+
 	</div>
 
-	<div class="panel" id="roommates-panel">
-		<div class="panel-heading">
-
-			<?= $dorm[0]["dorm_name"] ?>
-			
-		</div>
-		<div class="panel-body">
-			<ul class="list-unstyled">
-				<?php
-					foreach ($roomMates as $roommate) 
-					{
-						echo(	'<li>' .
+	<div id="roommates-list">
+		<legend align="center">Roommates</legend>
+		<ul class="list-unstyled">
+			<?php
+				foreach ($roomMates as $roommate) 
+				{
+					echo(	'<li>' .
 								$roommate["first_name"] .
-								'</li>'
-							);
-					}
-				?>
-			</ul>
-		</div> <!-- closes panel-body -->
-	</div> <!-- closes #roommates-panel -->
+								' ' .
+								$roommate["last_name"] .
+							'</li>'
+						);
+				}
+			?>
+		</ul>
+
+	</div>
 
 </div> <!-- close navbar column -->
