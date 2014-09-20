9a0012dd3905b3368c6b71ca7c5e19f8d714101d
Simon Verhoek <saverhoek@gmail.com>
2014-09-20 13:53:44 +0200

    removed redundant queries from navbar

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -8,27 +8,18 @@
 	//show navbar only when logged in
 	if (!preg_match("{(?:login|register|getdorm)\.php$}", $_SERVER["PHP_SELF"]))
 	{
-		// query user's data
-		$user = query("	SELECT 	first_name,
-								last_name,
-								cash_balance,
-								dorm_id 
-						FROM 	users 
-						WHERE 	user_id = ?", 
-						$_SESSION["user_id"]);
-
 		// query all users that are member of same dorm
 		$rows = query("	SELECT 	first_name, 
 								last_name 
 						FROM 	users 
 						WHERE 	dorm_id = ?", 
-						$user[0]["dorm_id"]);
+						$_SESSION["dorm_id"]);
 
 		// query dorm name
 		$dorm = query("	SELECT 	dorm_name 
 						FROM 	dorms 
 						WHERE 	dorm_id =?", 
-						$user[0]["dorm_id"]);
+						$_SESSION["dorm_id"]);
 
 		$roomMates = [];
 
