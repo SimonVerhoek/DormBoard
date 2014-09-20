4ce8583269e7db1dca9a9eccd349029dac0b70d7
Simon Verhoek <saverhoek@gmail.com>
2014-09-21 00:03:12 +0200

    cleaned navbar.php

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -9,22 +9,11 @@
 	if (!preg_match("{(?:login|register|getdorm)\.php$}", $_SERVER["PHP_SELF"]))
 	{
 		// query all users that are member of same dorm
-		$rows = query("	SELECT 	first_name, 
-								last_name 
-						FROM 	users 
-						WHERE 	dorm_id = ?", 
-						$_SESSION["dorm_id"]);
-
-		$roomMates = [];
-
-		// for each roommate
-		foreach ($rows as $roomMate)
-		{
-			$roomMates[] = [
-			"first_name" => $roomMate["first_name"],
-			"last_name" => $roomMate["last_name"]
-			]; 
-		}
+		$roomMates = query("	SELECT 	first_name, 
+										last_name 
+								FROM 	users 
+								WHERE 	dorm_id = ?", 
+										$_SESSION["dorm_id"]);
 
 		build("../templates/shownavbar.php", [
 			"dormName" => $_SESSION["dorm_name"],
