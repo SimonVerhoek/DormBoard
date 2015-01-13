72c179f7813b7225a103e222bd4d40e3ad339618
Simon Verhoek <saverhoek@gmail.com>
2015-01-13 16:10:09 +0100

    moved date_dropdown function from functions to functions_display

--- includes/functions.php
+++ includes/functions.php
@@ -200,42 +200,6 @@
     }
 
     /**
-     * Creates a dropdown menu for picking a date of birth.
-     */
-
-    function date_dropdown($year_limit = 1900)
-    {
-        $html_output = '<div id="date_select" >'."\n";
-
-        // days
-        $html_output .= '<select name="date_day" id="day_select">'."\n";
-            for ($day = 1; $day <= 31; $day++) {
-                $html_output .= '<option>' . $day . '</option>'."\n";
-            }
-        $html_output .= '</select>'."\n";
-
-        // months
-        $html_output .= '<select name="date_month" id="month_select" >'."\n";
-        $months = array("", "January", "February", "March", "April", 
-                            "May", "June", "July", "August", 
-                            "September", "October", "November", "December");
-            for ($month = 1; $month <= 12; $month++) {
-                $html_output .= '<option value="' . $month . '">' . $months[$month] . '</option>'."\n";
-            }
-        $html_output .= '</select>'."\n";
-
-        // years
-        $html_output .= '<select name="date_year" id="year_select">'."\n";
-            for ($year = date("Y"); $year >= $year_limit; $year--) {
-                $html_output .= '<option>' . $year . '</option>'."\n";
-            }
-        $html_output .= '</select>'."\n";
-
-        $html_output .= '</div>'."\n";
-        return $html_output;
-    }
-
-    /**
      * Gets user data and stores it in session.
      */
     function getUserData($userID)
